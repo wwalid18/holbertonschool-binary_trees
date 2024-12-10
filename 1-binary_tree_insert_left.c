7e88475d379a3a5a207ed0e6dccc7e15043b06d6
wwalid18 <9833@holbertonstudents.com>
2024-12-10 11:10:21 +0100

    tasks 1 to 3

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,11 +12,17 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
+	if (parent == NULL)
+		return (NULL);
 	new_node = malloc(sizeof(binary_tree_t));
 	if (!new_node)
 		return (NULL);
-	new_node->parent = parent;
-	parent->left = new_node;
 	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = parent->left;
+	new_node->right = NULL;
+	if (parent->left != NULL)
+		parent->left->parent = new_node;
+	parent->left = new_node;
 	return (new_node);
 }
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -12,11 +12,17 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
+	if (parent == NULL)
+		return (NULL);
 	new_node = malloc(sizeof(binary_tree_t));
 	if (!new_node)
 		return (NULL);
-	new_node->parent = parent;
-	parent->right = new_node;
 	new_node->n = value;
+	new_node->parent = parent;
+	new_node->right = parent->left;
+	new_node->left = NULL;
+	if (parent->right != NULL)
+		parent->right->parent = new_node;
+	parent->right = new_node;
 	return (new_node);
 }
