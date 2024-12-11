66001c7a5b1c981f78505860514d4e94bcfc2169
wwalid18 <9833@holbertonstudents.com>
2024-12-11 11:38:38 +0100

    task2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -19,7 +19,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		return (NULL);
 	new_node->n = value;
 	new_node->parent = parent;
-	new_node->right = parent->left;
+	new_node->right = parent->right;
 	new_node->left = NULL;
 	if (parent->right != NULL)
 		parent->right->parent = new_node;
